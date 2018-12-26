ee5a05dad910922881fc882a7522ac8de1372341
fain182 <fain182@gmail.com>
2018-12-26 18:01:11 +0100

    Added missing exception

---
+++ src/RequestException.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace Diciotto;
+
+
+use Psr\Http\Client\RequestExceptionInterface;
+use Psr\Http\Message\RequestInterface;
+
+class RequestException extends \RuntimeException implements RequestExceptionInterface
+{
+
+    private $request;
+
+    public function __construct(string $message, RequestInterface $request)
+    {
+        parent::__construct($message, 0, null);
+        $this->request = $request;
+    }
+
+    /**
+     * Returns the request.
+     *
+     * The request object MAY be a different object from the one passed to ClientInterface::sendRequest()
+     *
+     * @return RequestInterface
+     */
+    public function getRequest(): RequestInterface
+    {
+        return $this->request;
+    }
+
+}
